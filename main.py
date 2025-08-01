17c014c282e05a8828e472661633317a93e4a068
ArnoFF <arno.frf@gmail.com>
2025-08-01 07:58:34 +0200

    start with report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sort_char_list
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,10 +6,16 @@ def get_book_text(path):
 
 def main():
     path = "books/frankenstein.txt"
+    print(f"============ BOOKBOT ============ \n Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
     get_num_words(get_book_text(path))
+    
     characters = get_num_characters(get_book_text(path))
     for key in characters:
         print(f"'{key}': {characters[key]}")
+    
+
+    
 
 main()
 
--- stats.py
+++ stats.py
@@ -13,3 +13,10 @@ def get_num_characters(text):
             characters[letter] = 1
     return characters
     
+def sort_char_list(character_list):
+    new_char_list = []
+    for key in character_list:
+        dict_entry = {"char": key, "num": character_list[key]}
+        new_char_list.append(dict_entry)
+    new_char_list.sort()
+    print(new_char_list)
